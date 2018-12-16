8e3bd178d6726859b390808fe36fae4fcbcbb554
limgeun <shing100@naver.com>
2018-12-16 23:03:21 +0900

    영화 상세보기

--- pages/movie.js
+++ pages/movie.js
@@ -1,22 +1,31 @@
 import Head from "next/head";
 import { withRouter } from "next/router";
+import Axios from "axios";
 
 const Movie = (props) => (
     <div>
         <Head>
-            <title>{props.router.query.title} | Lim Store</title>
+            <title>{props.movie.title} | Lim Store</title>
         </Head>  
         <h1>
-            {props.title}
+            {props.movie.title}
         </h1>
-        <p> lalallalala</p>
+        <p>{props.movie.description_intro}</p>
     </div>
 )
 
-Movie.getInitialProps = async () => {
-    return {
-        title: "Lolololololo"
-    };
+Movie.getInitialProps = async context => {
+    const {
+        query: { id }
+    } = context;
+    const {
+        data: {
+        data: { movie }
+        }
+    } = await Axios.get(
+        `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+    );
+    return { movie };
 };
 
 export default withRouter(Movie);
