874e568f3d8f944540bd7a3a4b49a728853c95ac
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2018-12-31 20:30:29 +0900

    serviceworker로 오프라인 지원하기 기능 추가

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,3 +1,20 @@
-setInterval(() => {
-    console.log("Hi!");
-}, 3000);
+self.addEventListener("install", (event) => {
+    const offlinePage = new Request("/");
+    event.waitUntill(
+        fetch(offlinePage).then(response => {
+            return caches.open("lim-store").then(cache => {
+                return cache.put(offlinePage, response);
+            });
+        })
+    );
+});
+
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        //fetch(event.requset).catch(() => console.log("Offline"))
+        fetch(event.request).catch(error => {
+            return caches.open("lim-store").then(cache => cache.match("/"));
+        })
+    );
+});
